1ee6c69cdbb059b84874880c3764d1340c0bb25f
cbush19 <cbush5400@gmail.com>
2019-12-22 22:49:37 -0500

    fix users table ref on seeders and migrations

--- migrations/20191204210945-add_income_cols.js
+++ migrations/20191204210945-add_income_cols.js
@@ -3,12 +3,12 @@
 module.exports = {
 
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('users', 'monthly_income', {
+    return queryInterface.addColumn('Users', 'monthly_income', {
       type: Sequelize.DECIMAL
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('users', 'monthly_income');
+    return queryInterface.removeColumn('Users', 'monthly_income');
   },
 
 };
